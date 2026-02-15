9369b51d1148ffd524a92fa9dbdc9f725163e988
Maseed Irfan Ali <61617374+MaseedIrfanAli@users.noreply.github.com>
2026-02-15 17:41:51 +0530

    Update job name and table references in DAG

--- bq_to_bq_dataflow_dag.py
+++ bq_to_bq_dataflow_dag.py
@@ -12,7 +12,6 @@ TEMPLATE_GCS_PATH = (
     f"gs://interview-dataflow-templates/bq_to_bq_build-{BUILD_VERSION}.json"
 )
 
-
 default_args = {
     "start_date": datetime(2024, 1, 1),
     "retries": 1,
@@ -32,11 +31,15 @@ with DAG(
         location=REGION,
         body={
             "launchParameter": {
-                "jobName": "bq-to-bq-build-14",
+                "jobName": f"bq-to-bq-build-{BUILD_VERSION}",
                 "containerSpecGcsPath": TEMPLATE_GCS_PATH,
                 "parameters": {
-                    "input_table": "bamboo-autumn-484913-i0:dataset.input_table",
-                    "output_table": "bamboo-autumn-484913-i0:dataset.output_table"
+                    "input_table": "bamboo-autumn-484913-i0.dataset.sales_raw",
+                    "output_table": "bamboo-autumn-484913-i0.dataset.sales"
+                },
+                "environment": {
+                    "tempLocation": "gs://interview-dataflow-templates/temp/",
+                    "stagingLocation": "gs://interview-dataflow-templates/staging/"
                 }
             }
         }
