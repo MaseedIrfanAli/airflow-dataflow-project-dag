cdbb5c706976efe0b30045e8175d570856cadb18
Maseed Irfan Ali <61617374+MaseedIrfanAli@users.noreply.github.com>
2026-02-16 19:01:09 +0530

    Update bq_to_bq_dataflow_dag.py

--- bq_to_bq_dataflow_dag.py
+++ bq_to_bq_dataflow_dag.py
@@ -8,25 +8,16 @@ REGION = "us-central1"
 
 BUILD_VERSION = Variable.get("dataflow_build_version")
 
-TEMPLATE_GCS_PATH = (
-    f"gs://interview-dataflow-templates/bq_to_bq_build-{BUILD_VERSION}.json"
-    
-)
-
-default_args = {
-    "start_date": datetime(2024, 1, 1),
-    "retries": 1,
-}
+TEMPLATE_GCS_PATH = f"gs://interview-dataflow-templates/bq_to_bq_build-{BUILD_VERSION}.json"
 
 with DAG(
     dag_id="bq_to_bq_flex_template",
-    default_args=default_args,
-    schedule_interval=None,
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
     catchup=False,
-    tags=["dataflow", "flex", "bq"]
 ) as dag:
 
-    start_flex = DataflowStartFlexTemplateOperator(
+    run_flex = DataflowStartFlexTemplateOperator(
         task_id="run_bq_to_bq_flex",
         project_id=PROJECT_ID,
         location=REGION,
@@ -35,13 +26,12 @@ with DAG(
                 "jobName": f"bq-to-bq-build-{BUILD_VERSION}",
                 "containerSpecGcsPath": TEMPLATE_GCS_PATH,
                 "parameters": {
-                    "input_table": "bamboo-autumn-484913-i0.dataset.sales_raw",
-                    "output_table": "bamboo-autumn-484913-i0.dataset.sales"
+                    "input_table": "bamboo-autumn-484913-i0:dataset.sales_raw",
+                    "output_table": "bamboo-autumn-484913-i0:dataset.sales"
                 },
                 "environment": {
-                    "tempLocation": "gs://interview-dataflow-templates/temp/",
-                    "stagingLocation": "gs://interview-dataflow-templates/staging/"
-                   
+                    "tempLocation": "gs://interview-dataflow-templates/temp",
+                    "stagingLocation": "gs://interview-dataflow-templates/staging"
                 }
             }
         }
